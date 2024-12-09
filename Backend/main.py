9b1bd49980a74536a58ff6f6e1b9413d85c41037
minhduckd5 <minhduc.kd3004@gmail.com>
2024-12-10 01:22:43 +0700

    .

--- Backend/main.py
+++ Backend/main.py
@@ -821,17 +821,17 @@ async def get_cart(
                 for item in cart.items
             ]
         }
-        if type == "remove-all":
-            db.query(ShoppingCartItem).delete()  # Remove all items from the cart
-            db.commit()
-            return {"message": "All items removed from cart."}
-        if type == "update-quantity":
-            cart_item = db.query(ShoppingCartItem).filter(ShoppingCartItem.product_item_id == product_id).first()
-            if not cart_item:
-                raise HTTPException(status_code=404, detail="Item not found in cart")
-            cart_item.quantity = quantity
-            db.commit()
-            return {"message": "Quantity updated successfully"}
+    if type == "remove-all":
+        db.query(ShoppingCartItem).delete()  # Remove all items from the cart
+        db.commit()
+        return {"message": "All items removed from cart."}
+    if type == "update-quantity":
+        cart_item = db.query(ShoppingCartItem).filter(ShoppingCartItem.product_item_id == product_id).first()
+        if not cart_item:
+            raise HTTPException(status_code=404, detail="Item not found in cart")
+        cart_item.quantity = quantity
+        db.commit()
+        return {"message": "Quantity updated successfully"}
 # ------------------------------
 # Remove Item from Cart
 # ------------------------------
